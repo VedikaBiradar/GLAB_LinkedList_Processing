4e42756be933e587df0506eb50f5d583ac0eddd5
VedikaBiradar <vedikabiradar2001@gmail.com>
2024-05-02 18:56:48 -0400

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+      LinkedListExample ll = new LinkedListExample();
+      ll.linkedListExample();
+
+
+      SortingLL sl = new SortingLL();
+      sl.sortLL();
+
+    }
+}
